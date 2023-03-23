64df774c75baab80e04325b8521db2fe7cb373f6
Mohamed Elsayed Ali <mohamedelsayed20258@gmail.com>
2023-03-23 16:49:31 +0200

    'Router'

--- src/modules/jobs/jobs.router.js
+++ src/modules/jobs/jobs.router.js
@@ -2,7 +2,7 @@
 
 import express from 'express'
 import auth from '../../middleware/authentication.js'
-import { getAllJobs } from './jobs.controller.js'
+import { getAllJobs , getJob , createJob , updateJob , deleteJob , showStats} from './jobs.controller.js'
 import testUser from '../../middleware/testUser.js'
 
 
@@ -11,6 +11,11 @@ const JobsRouter = express.Router()
 // Demo user read only can not to create or update or delete 
 
 JobsRouter.get('/getAllJobs' , auth, testUser , getAllJobs)
+JobsRouter.get('/getJob/:id' , auth,  getJob)
+JobsRouter.post('/createJob' , auth , createJob)
+JobsRouter.patch('/updateJob/:id' , auth , updateJob)
+JobsRouter.delete('/deleteJob/:id' , auth , deleteJob)
+JobsRouter.get('/showStats' , auth , showStats)
 
 
 export default JobsRouter
